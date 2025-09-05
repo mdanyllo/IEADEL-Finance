a46a5c8b7c15d02daf8e9621af10a80bcb1c9082
Paulo Andre Correa Pereira <JT0009347@mateus.dc>
2025-09-05 15:29:41 -0300

    adição de logout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function NavBar() {
+    async function handleLogout() {
+        await fetch("/api/logout", { method: "POST" });
+        window.location.href = "/";
+    }
     return (
         <div className="flex w-full py-1 shadow-md">
             <div className="flex flex-row justify-between items-center m-4 w-full">
@@ -18,15 +22,14 @@ export default function NavBar() {
             </Image>
             </Link>
 
-            <Link href="/">
-            <Image 
+            <button onClick={handleLogout}>
+            <Image
                 src="/perfil.png"
                 alt="Perfil"
                 width={40}
                 height={40}
-                >
-            </Image>
-            </Link>
+            />
+            </button>
             </div>
         </div>
     )
