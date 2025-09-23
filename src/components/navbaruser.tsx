5fa50c4196b40a88acfe0f04064796eff42199a9
mdanyllo <markusdanyllo07@gmail.com>
2025-09-23 15:01:41 -0300

    nome personalizado

--- src/components/navbaruser.tsx
+++ src/components/navbaruser.tsx
@@ -3,12 +3,26 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function NavBarUser() {
     async function handleLogout() {
         await fetch("/api/logout", { method: "POST" });
         window.location.href = "/";
     }
+
+    const [nome, setNome] = useState("");
+    
+    useEffect(() => {
+        const userData = localStorage.getItem("user");
+        if (userData) {
+            const user = JSON.parse(userData);
+            const nomeCompleto = user.nome.split(" ");
+            const primeirosNomes = nomeCompleto.slice(0, 2).join(" ")
+            setNome(primeirosNomes);
+        }
+    }, []);
+
     return (
         <div className="flex w-full py-1 shadow-md">
             <div className="flex flex-row justify-between items-center m-4 w-full">
@@ -21,6 +35,7 @@ export default function NavBarUser() {
                 >
             </Image>
             </Link>
+            <h1 className="text-center font-semibold text-sm md:text-xl">Bem-vindo, {nome ? nome : "Visitante"}</h1>
             <button title="Sair" className="cursor-pointer" onClick={handleLogout}>
             <Image
                 src="/logout.png"
