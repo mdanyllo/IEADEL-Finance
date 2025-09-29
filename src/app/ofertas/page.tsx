4e9e517e228adf53e7849cf0af36721be51e3663
Paulo Andre Correa Pereira <Jt0009347@MATEUS.DC>
2025-09-29 10:05:33 -0300

    correção em implementação de ofertas

--- src/app/ofertas/page.tsx
+++ src/app/ofertas/page.tsx
@@ -1,7 +1,10 @@
 import NavBar from "@/components/navbar"
 import MonthSelector from "@/components/monthselector"
-import Movimentacoes from "@/components/movimentacoesofertas"
+import Movimentacoes, { Oferta } from "@/components/movimentacoesofertas"
 import FormOfertas from "@/components/formofertas"
+import { useState } from "react"
+import { useEffect } from "react"
+
 
 export const metadata = {
   title: "Ofertas",
@@ -9,12 +12,47 @@ export const metadata = {
 
 
 export default function Ofertas() {
+    const [mes, setMes] = useState(new Date().getMonth() + 1);
+    const [ano, setAno] = useState(new Date().getFullYear());
+    const [oferta, setOferta] = useState<Oferta[]>([]);
+
+    function getIdCongregacao(): number | null {
+            try {
+                const user = localStorage.getItem("user");
+                return user ? JSON.parse(user).congregacao.idCongregacao : null;
+            } catch (error) {
+                console.error("Erro ao ler localStorage:", error);
+                return null;
+            }
+        }
+        useEffect(() => {
+            async function fetchDizimos() {
+                try {
+                    const idCongregacao = getIdCongregacao();
+                    const mesFormatado = String(mes).padStart(2, "0");
+                    const res = await fetch(`/api/movimentacoes/dizimos/porCongregacao?tipo=OFERTA&mes=${mesFormatado}&ano=${ano}&idCongregacao=${idCongregacao}`);
+                    const data = await res.json();
+                    setOferta(data);
+                } catch (err) {
+                    console.error("Erro ao carregar dízimos:", err);
+                }
+            }
+            fetchDizimos();
+        }, [mes, ano]);
     return (
         <>
             <div>
                 <NavBar />
                 <MonthSelector />
-                <Movimentacoes />
+                <Movimentacoes 
+                    initialMonth={mes}
+                    initialYear={ano}
+                    ofertas={oferta}
+                    onChange={(novoMes, novoAno) => {
+                        setMes(novoMes);
+                        setAno(novoAno);
+                    }}
+                />
                 <FormOfertas />
             </div>
         </>
